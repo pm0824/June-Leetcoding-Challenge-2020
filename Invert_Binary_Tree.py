e7f26b8a58ff3c5ee60637e2a3011ed136cb7f10
Prachi Mane <46194962+pm0824@users.noreply.github.com>
2020-06-01 16:42:30 +0530

    Day 1

---
+++ Invert_Binary_Tree.py
@@ -0,0 +1,59 @@
+'''
+Invert a binary tree.
+Example:
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+'''
+
+#Solution 1: Using Recursion
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
+        
+#Solution 2: Using BFS
+def invertTree(self, root):
+    queue = collections.deque([(root)])
+    while queue:
+        node = queue.popleft()
+        if node:
+            node.left, node.right = node.right, node.left
+            queue.append(node.left)
+            queue.append(node.right)
+    return root
+    
+#Solution 3: Using DFS
+def invertTree(self, root):
+    stack = [root]
+    while stack:
+        node = stack.pop()
+        if node:
+            node.left, node.right = node.right, node.left
+            stack.extend([node.right, node.left])
+    return root
+    
+    
+    
+    
+    
