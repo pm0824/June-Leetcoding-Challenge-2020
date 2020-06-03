541cfc5ad4e47bfdaae987e6e1409f466ddeab44
Prachi Mane <46194962+pm0824@users.noreply.github.com>
2020-06-03 20:24:20 +0530

    Day 3

---
+++ Two_City_Scheduling.py
@@ -0,0 +1,35 @@
+'''
+There are 2N people a company is planning to interview. The cost of flying the i-th person to city A is costs[i][0], 
+and the cost of flying the i-th person to city B is costs[i][1].
+Return the minimum cost to fly every person to a city such that exactly N people arrive in each city.
+
+Example 1:
+Input: [[10,20],[30,200],[400,50],[30,20]]
+Output: 110
+Explanation: 
+The first person goes to city A for a cost of 10.
+The second person goes to city A for a cost of 30.
+The third person goes to city B for a cost of 50.
+The fourth person goes to city B for a cost of 20.
+The total minimum cost is 10 + 30 + 50 + 20 = 110 to have half the people interviewing in each city.
+ 
+Note:
+1. 1 <= costs.length <= 100
+2. It is guaranteed that costs.length is even.
+3. 1 <= costs[i][0], costs[i][1] <= 1000
+'''
+#Solution: Using Greedy Algorithm
+
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        res = 0
+        c = sorted(costs, key=lambda x : x[0] - x[1])
+        for i in range(0,len(c)//2):
+                res += c[i][0]
+        for i in range(len(c)//2,len(c)):
+                res += c[i][1]
+        return res
+        
+        
+        
+        
